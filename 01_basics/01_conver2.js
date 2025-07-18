5823409b05ef58779d278f654e23408341245246
Ganavi-ch <chganavi70@gmail.com>
2025-07-18 15:44:52 +0000

    conversion

---
+++ 01_basics/01_conver2.js
@@ -0,0 +1,30 @@
+let num=2;
+let num1= -num;
+console.log(num1);// -2
+
+console.log(2**2)// power of 2
+
+let nam="Gana"
+let nam2="vi"
+console.log(nam+nam2)// Ganavi
+
+console.log("2"+2+2);// 222 all string
+console.log(2+2+"2")// 42 first two number 
+
+let x = 5;
+const y = x++;
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:6, y:5"
+
+let a = 3;
+const b = ++a;
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+console.log(+true)// conversion using concatenation//1
+console.log(+"")//0
+
+let gc=100;
+console.log(++gc);
+console.log(gc++)
---
+++ 01_basics/01_conversion.js
@@ -0,0 +1,16 @@
+let score=33;
+console.log(typeof(score))//method
+
+let name="abc";
+let isnum=Number(name)
+console.log(typeof(isnum))
+console.log(isnum)// NaN=not a number
+
+let isboolean= Boolean(name)
+console.log(isboolean)// true
+
+/*
+  bool 1=true,0=false, ""=false, "xyz"=true, "33"=33;
+  number null=0; undefined=nan, string=nan;
+  string 33=33
+*/ 
