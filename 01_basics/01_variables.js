4c09650623f82a71a57a8d7ece5daf6c008f8d4d
Ganavi-ch <chganavi70@gmail.com>
2025-07-18 13:03:15 +0000

    Add 01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const id=123; /*cant change */
+let acc="ganavi";
+var num="wed";//block/functional scope not identifible
+accountCity="Hassan";
+
+//id=3 not allowed
+
+let ganu;//undefined 
+
+acc="kushi";
+
+console.log(id);
+console.table([acc,num,accountCity])
